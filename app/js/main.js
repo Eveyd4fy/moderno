d8c85e1d10e12c8c1a36c0ee6fadc85e00454240
Eveyd4fy <nkudlatskiy@mail.ru>
2020-06-14 20:24:16 +0700

    adapted header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -26,12 +26,21 @@ $(function() {
         $('.products__item').addClass('list');
         $('.icon-th-list').addClass('active');
         $('.icon-th-large').removeClass('active');
+        // console.log("12312");
     });
 
     $('.icon-th-large').on('click', function () {
         $('.products__item').removeClass('list')
         $('.icon-th-large').addClass('active');
         $('.icon-th-list').removeClass('active');
+    });
+   
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function () {
+        $('.header__box').toggleClass('active');
     });
 
     var mixer = mixitup('.products__inner-box');
